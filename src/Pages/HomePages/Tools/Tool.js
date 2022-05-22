7d88e1e736c91e93fcef97d7eb0acc5871e54160
witerowson <runurowson@gmail.com>
2022-05-22 19:47:39 +0600

    tools

--- src/Pages/HomePages/Tools/Tool.js
+++ src/Pages/HomePages/Tools/Tool.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
-const Tool = () => {
+const Tool = ({ tool }) => {
+
+    const { _id, img, name, price, description, brand, quantity, other } = tool
     return (
-        <div>
-            <h1> This tools</h1>
-        </div>
+
+        <div class="card w-96 bg-base-100 shadow-xl my-8">
+            <figure className="px-10 pt-10">
+                <img src={img} alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 className="card-title text-2xl">{name}</h2>
+                <p className='text-left text-sm'>{description}</p>
+                <p><b>Brand: </b>{brand}</p>
+                <p><b>Quantity: </b>{quantity} pieces</p>
+                <p><b>Price: </b>{price}</p>
+                <div class="card-actions">
+                    <button className="btn btn-secondary btn-sm animate-bounce w-40 h-6 hover:btn-primary">Order Now</button>
+                </div>
+            </div >
+
+        </div >
     );
 };
 
--- src/Pages/HomePages/Tools/Tools.js
+++ src/Pages/HomePages/Tools/Tools.js
@@ -7,8 +7,17 @@ const Tools = () => {
 
 
     return (
-        <div>
-            <Tool></Tool>
+        <div >
+            <h1 className='text-4xl text-center text-primary mt-5 underline underline-offset-4'> Tools and Accessories</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-3  gap-4 drop-shadow-lg mx-8'>
+                {
+                    Tools.map(tool => <Tool
+                        key={tool._id}
+                        tool={tool}
+                    ></Tool>)
+                }
+            </div>
+
         </div>
     );
 };
